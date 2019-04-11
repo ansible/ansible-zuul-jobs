6afde03d0239f00cd7ab4e93b4a2f2a4125a1539
Paul Belanger <pabelanger@redhat.com>
2019-04-11 19:25:34 -0400

    Create ansible-network-vyos-base job
    This will be our base job for vyos related jobs. We are creating a 2
    node nodeset with fedora-29 and vyos. We also modify our base job so it
    will not run against the appliance group.
    
    Depends-On: https://review.openstack.org/652424
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

---
+++ roles/write-inventory-fork/library/test_write_inventory.py
@@ -0,0 +1,163 @@
+# Copyright (C) 2018 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+#
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+import testtools
+import fixtures
+import os
+
+import yaml
+
+from .write_inventory import run
+
+INPUT = yaml.safe_load("""
+bionic:
+  ansible_connection: ssh
+  ansible_host: 104.130.217.77
+  ansible_port: 22
+  ansible_user: zuul
+  nodepool:
+    az: null
+    cloud: rax
+    interface_ip: 104.130.217.77
+    label: ubuntu-bionic
+    private_ipv4: 10.210.196.115
+    provider: rax-ord
+    public_ipv4: 104.130.217.77
+    public_ipv6: 2001:4801:7828:101:be76:4eff:fe10:14eb
+    region: ORD
+xenial:
+  ansible_connection: ssh
+  ansible_host: 149.202.170.85
+  ansible_port: 22
+  ansible_user: zuul
+  nodepool:
+    az: nova
+    cloud: ovh
+    interface_ip: 149.202.170.85
+    label: ubuntu-xenial
+    private_ipv4: 149.202.170.85
+    provider: ovh-gra1
+    public_ipv4: 149.202.170.85
+    public_ipv6: 2001:41d0:302:1000::17:a32b
+    region: GRA1
+""")
+
+GROUPS_INPUT = yaml.safe_load("""
+all: []
+ungrouped: []
+puppet:
+  - bionic
+  - xenial
+""")
+
+
+class TestWriteInventory(testtools.TestCase):
+    def assertOutput(self, dest, ref):
+        with open(dest) as f:
+            out = yaml.safe_load(f)
+        self.assertEqual(ref, out)
+
+    def test_all(self):
+        '''Test passing all variables'''
+        dest = self.useFixture(fixtures.TempDir()).path
+        dest = os.path.join(dest, 'out.yaml')
+        run(dest, INPUT, GROUPS_INPUT, None, None)
+
+        self.assertOutput(dest, {
+            'all': {
+                'children': {
+                    'puppet': {
+                        'hosts': {
+                            'bionic': None,
+                            'xenial': None,
+                        },
+                    },
+                },
+                'hosts': {
+                    'bionic': {
+                        "ansible_connection": "ssh",
+                        "ansible_user": "zuul",
+                        "ansible_host": "104.130.217.77",
+                        "ansible_port": 22
+                    },
+                    'xenial': {
+                        "ansible_connection": "ssh",
+                        "ansible_user": "zuul",
+                        "ansible_host": "149.202.170.85",
+                        "ansible_port": 22,
+                    }
+                }
+            }
+        })
+
+    def test_include(self):
+        '''Test incuding vars'''
+        dest = self.useFixture(fixtures.TempDir()).path
+        dest = os.path.join(dest, 'out.yaml')
+        run(dest, INPUT, GROUPS_INPUT, ['ansible_host'], None)
+
+        self.assertOutput(dest, {
+            'all': {
+                'children': {
+                    'puppet': {
+                        'hosts': {
+                            'bionic': None,
+                            'xenial': None,
+                        },
+                    },
+                },
+                'hosts': {
+                    'bionic': {
+                        "ansible_host": "104.130.217.77",
+                    },
+                    'xenial': {
+                        "ansible_host": "149.202.170.85",
+                    }
+                }
+            }
+        })
+
+    def test_exclude(self):
+        '''Test passing all variables'''
+        dest = self.useFixture(fixtures.TempDir()).path
+        dest = os.path.join(dest, 'out.yaml')
+        run(dest, INPUT, GROUPS_INPUT, None, ['ansible_user'])
+
+        self.assertOutput(dest, {
+            'all': {
+                'children': {
+                    'puppet': {
+                        'hosts': {
+                            'bionic': None,
+                            'xenial': None,
+                        },
+                    },
+                },
+                'hosts': {
+                    'bionic': {
+                        "ansible_connection": "ssh",
+                        "ansible_host": "104.130.217.77",
+                        "ansible_port": 22
+                    },
+                    'xenial': {
+                        "ansible_connection": "ssh",
+                        "ansible_host": "149.202.170.85",
+                        "ansible_port": 22,
+                    }
+                }
+            }
+        })
---
+++ roles/write-inventory-fork/library/write_inventory.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+#
+# Copyright 2018 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import json
+
+from ansible.module_utils.basic import AnsibleModule
+
+# The list of variables we might include
+VARS = [
+    'ansible_python_interpreter',
+    'ansible_connection',
+    'ansible_host',
+    'ansible_network_os',
+    'ansible_port',
+    'ansible_user'
+]
+
+
+def run(dest, hostvars, groups, include, exclude):
+    children = {}
+    for group, hostnames in groups.items():
+        if group == 'all' or group == 'ungrouped':
+            continue
+        children[group] = {}
+        children[group]['hosts'] = {}
+        for host in hostnames:
+            children[group]['hosts'][host] = None
+
+    out_all = {}
+    out = {
+        'all': {
+            'children': children,
+            'hosts': out_all
+        }
+    }
+    for host, hvars in hostvars.items():
+        d = {}
+        for v in VARS:
+            if v not in hvars:
+                continue
+            if include is not None:
+                if v not in include:
+                    continue
+            if exclude is not None:
+                if v in exclude:
+                    continue
+            d[v] = hvars[v]
+        out_all[host] = d
+
+    with open(dest, 'w') as f:
+        f.write(json.dumps(out))
+
+
+def ansible_main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            dest=dict(required=True, type='path'),
+            hostvars=dict(required=True, type='raw'),
+            groups=dict(required=True, type='raw'),
+            include_hostvars=dict(type='list'),
+            exclude_hostvars=dict(type='list'),
+        )
+    )
+
+    p = module.params
+
+    dest = p.get('dest')
+    hostvars = p.get('hostvars')
+    groups = p.get('groups')
+    include = p.get('include_hostvars')
+    exclude = p.get('exclude_hostvars')
+
+    run(dest, hostvars, groups, include, exclude)
+
+    module.exit_json(changed=True)
+
+
+if __name__ == '__main__':
+    ansible_main()

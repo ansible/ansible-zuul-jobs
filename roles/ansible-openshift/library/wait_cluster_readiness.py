f11029c38e9ab188d2985fe73441ae4587865fb9
abikouo <abikouo@redhat.com>
2022-06-23 18:35:30 +0200

    add node status to better debug cluster readiness

--- roles/ansible-openshift/library/wait_cluster_readiness.py
+++ roles/ansible-openshift/library/wait_cluster_readiness.py
@@ -4,6 +4,7 @@
 # Copyright: (c) 2022, Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 from __future__ import absolute_import, division, print_function
+from platform import node
 
 __metaclass__ = type
 
@@ -42,6 +43,21 @@ from ansible.module_utils.basic import AnsibleModule
 import json
 
 
+def get_node_status(module):
+    cmd = [module.params.get("binary_path"), "get", "nodes", "-o", "json"]
+    rc, out, err = module.run_command(cmd, environ_update=dict(KUBECONFIG=module.params.get("kubeconfig")))
+    result = []
+    if rc != 0:
+        return result
+    for node in json.loads(out).get("items", []):
+        name = node["metadata"]["name"]
+        for st in node["status"]["conditions"]:
+            if st["type"] == "Ready":
+                result.append(dict(node=name, msg=st.get("message"), ready=bool(st["status"] == "True")))
+                break
+    return result
+
+
 def test_cluster_readiness(module):
     cmd = [module.params.get("binary_path"), "get", "clusterversion", "-o", "json"]
     rc, out, err = module.run_command(cmd, environ_update=dict(KUBECONFIG=module.params.get("kubeconfig")))
@@ -50,9 +66,9 @@ def test_cluster_readiness(module):
     result = json.loads(out)["items"][0]
     history = result["status"]["history"][0]
     if history.get("state") == "Completed":
-        return True, "Installation completed"
+        return True, "Installation completed", get_node_status(module)
     else:
-        return False, "".join([x["message"] for x in result["status"]["conditions"] if x["type"] == "Progressing"])
+        return False, "".join([x["message"] for x in result["status"]["conditions"] if x["type"] == "Progressing"]), get_node_status(module)
 
 
 def main():
@@ -63,8 +79,8 @@ def main():
         ),
     )
 
-    completed, msg = test_cluster_readiness(module)
-    module.exit_json(completed=completed, msg=msg)
+    completed, msg, nodes = test_cluster_readiness(module)
+    module.exit_json(completed=completed, msg=msg, nodes=nodes)
 
 
 if __name__ == "__main__":
